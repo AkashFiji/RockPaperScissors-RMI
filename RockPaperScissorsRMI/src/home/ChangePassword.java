d1636710feb6138928a061ac9a0e826478c8bceb
Akash <137357824+AkashFiji@users.noreply.github.com>
2024-10-23 11:25:56 +1200

    Update ChangePassword.java

--- RockPaperScissorsRMI/src/home/ChangePassword.java
+++ RockPaperScissorsRMI/src/home/ChangePassword.java
@@ -11,10 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 /**
- * @author kavish chandra - S11219143
- * @author Sudhansu Jayshil kisun - S11219520
- * @author Fardeen Ali - S11219171
- * @author Akash Mishra - S11219345
+ * @author Akash Mishra
  */
 
 public class ChangePassword extends javax.swing.JFrame {
