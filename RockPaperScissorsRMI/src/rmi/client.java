32497c7efb173d3178ad7f8a5056727ef96a7dd1
Akash <137357824+AkashFiji@users.noreply.github.com>
2024-10-23 11:28:24 +1200

    Update client.java

--- RockPaperScissorsRMI/src/rmi/client.java
+++ RockPaperScissorsRMI/src/rmi/client.java
@@ -10,10 +10,7 @@ import java.rmi.RemoteException;
 import java.rmi.server.ServerNotActiveException;
 
 /**
- * @author kavish chandra - S11219143
- * @author Sudhansu Jayshil kisun - S11219520
- * @author Fardeen Ali - S11219171
- * @author Akash Mishra - S11219345
+ * @author Akash Mishra
  */
 
 public class client {
